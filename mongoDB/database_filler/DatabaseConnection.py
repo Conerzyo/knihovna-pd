1dbcc81521d2dcae3f834445b0a411bd7b77f118
nothrax <trubka.jakub@gmail.com>
2023-01-09 21:05:19 +0100

    Loan create trigger

--- mongoDB/database_filler/DatabaseConnection.py
+++ mongoDB/database_filler/DatabaseConnection.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-
+import time
 
 class DatabaseConnection:
     database_name = "AK7PD"
@@ -12,22 +12,51 @@ class DatabaseConnection:
         self.client = MongoClient(connection_string)
 
     def drop_databases(self):
-        self.client[self.database_name].drop_collection(self.books_collection_name)
-        self.client[self.database_name].drop_collection(self.users_collection_name)
-        self.client[self.database_name].drop_collection(self.loans_collection_name)
+        self.client[self.database_name][self.books_collection_name].delete_many({})
+        self.client[self.database_name][self.users_collection_name].delete_many({})
+        self.client[self.database_name][self.loans_collection_name].delete_many({})
         return
 
     def add_book_structure(self, book):
         ret_value = self.client[self.database_name][self.books_collection_name].insert_one(book)
+        return
 
     def add_user_structure(self, user):
         ret_value = self.client[self.database_name][self.users_collection_name].insert_one(user)
+        return
 
     def add_loan_structure(self, loan):
-        book = loan["book"]
-        user = loan["user"]
-        entry = {"book": book, "user": user}
+        book = loan["title"]
+        user = loan["username"]
+        self.add_loan(book, user)
+        #entry = {"book": book, "user": user}
+        #ret_value = self.client[self.database_name][self.loans_collection_name].insert_one(entry)
+        #todo validation insert https://stackoverflow.com/questions/46569262/does-pymongo-have-validation-rules-built-in
+
+    def add_loan(self, title, user_name):
+        book = self.client[self.database_name][self.books_collection_name].find_one({"title": title})
+        user = self.client[self.database_name][self.users_collection_name].find_one({"username": user_name})
+
+        #todo check in triger?
+        if book is None:
+            print(f"[ERROR] Book {title} not found")
+            return False
+        if user is None:
+            print(f"[ERROR] User {user_name} not found")
+            return False
+        if book['countAvailable'] < 0 or not user['active']:
+            print(f"[ERROR] Not enough books {title}")
+            return False
+        if not user['active']:
+            print(f"[ERROR] User {user_name} is not active")
+            return False
+        if user['loanCount'] >= 6:
+            print(f"[ERROR] User {user_name} cannot loan more than 6 books")
+            return False
+
+        entry = {"book": book['_id'], "user": user['_id']} #todo get rid of constants, create model
         ret_value = self.client[self.database_name][self.loans_collection_name].insert_one(entry)
+        return True
 
     def create_triggers(self):
         return
--- mongoDB/database_filler/triggers/loan_create.js
+++ mongoDB/database_filler/triggers/loan_create.js
@@ -1,11 +1,14 @@
 exports = function(changeEvent) {
   mongodb = context.services.get("AK7PD")
-  console.log(mongodb)
-  collection = mongodb.db("AK7PD").collection("loans")
+  loans_coll = mongodb.db("AK7PD").collection("loans")
+  books_coll = mongodb.db("AK7PD").collection("books")
+  users_coll = mongodb.db("AK7PD").collection("users")
 
-  const loan_date = Math.floor(Date.now() / 1000)
-  const due_date = loan_date + 518400
+  const loan_date = new Date().getTime()
+  const due_date = loan_date + 300//518400
   var fullDocument = changeEvent.fullDocument;
   fullDocument = { ...fullDocument, loanDate: loan_date, dueDate: due_date}
-  collection.insertOne(fullDocument)
+  loans_coll.updateOne({_id: fullDocument._id}, {$set: fullDocument})
+  books_coll.updateOne({_id: fullDocument.book},{$inc: {"countAvailable": -1}})
+  users_coll.updateOne({_id: fullDocument.user},{$inc: {"loanCount": 1}})
 };
