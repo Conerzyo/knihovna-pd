44ebfcc545fbc8dee5ffb041c4e3748515d5334b
Jakub Trubka <trubka.jakub@gmail.com>
2023-01-25 14:59:40 +0100

    Update loan_create.js

--- mongoDB/database_filler/triggers/loan_create.js
+++ mongoDB/database_filler/triggers/loan_create.js
@@ -1,14 +1,19 @@
 exports = function(changeEvent) {
-  mongodb = context.services.get("AK7PD")
-  loans_coll = mongodb.db("AK7PD").collection("loans")
-  books_coll = mongodb.db("AK7PD").collection("books")
-  users_coll = mongodb.db("AK7PD").collection("users")
-
-  const loan_date = new Date().getTime()
-  const due_date = loan_date + 300//518400
-  var fullDocument = changeEvent.fullDocument;
-  fullDocument = { ...fullDocument, loanDate: loan_date, dueDate: due_date}
-  loans_coll.updateOne({_id: fullDocument._id}, {$set: fullDocument})
-  books_coll.updateOne({_id: fullDocument.book},{$inc: {"countAvailable": -1}})
-  users_coll.updateOne({_id: fullDocument.user},{$inc: {"loanCount": 1}})
+  mongodb = context.services.get("AK7PD");
+  loans_coll = mongodb.db("AK7PD").collection("loans");
+  books_coll = mongodb.db("AK7PD").collection("books");
+  users_coll = mongodb.db("AK7PD").collection("users");
+  
+  
+  notNew = doc.hasOwnProperty('loanDate');
+  if(!notNew){
+      const loan_date = new Date().getTime();
+      const due_date = loan_date + 300;//518400
+      var fullDocument = changeEvent.fullDocument;
+      fullDocument = { ...fullDocument, loanDate: loan_date, dueDate: due_date}
+      
+      loans_coll.updateOne({_id: fullDocument._id}, {$set: fullDocument})
+      books_coll.updateOne({_id: fullDocument.book},{$inc: {"countAvailable": -1}})
+      users_coll.updateOne({_id: fullDocument.user},{$inc: {"loanCount": 1}})
+  }
 };
