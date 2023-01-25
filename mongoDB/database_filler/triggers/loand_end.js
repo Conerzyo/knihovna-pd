aa4c132a8419ba6d69e2de4220a87f908d30b2b6
Jakub Trubka <trubka.jakub@gmail.com>
2023-01-25 14:56:51 +0100

    Create loand_end.js

---
+++ mongoDB/database_filler/triggers/loand_end.js
@@ -0,0 +1,25 @@
+exports = async function() {
+  mongodb = context.services.get("AK7PD")
+  loans_coll = mongodb.db("AK7PD").collection("loans")
+  books_coll = mongodb.db("AK7PD").collection("books");
+  users_coll = mongodb.db("AK7PD").collection("users");
+  
+  loans = await loans_coll.find({}).toArray().then(items => { return items}).catch(err => console.error(`Failed to find documents: ${err}`))
+  
+  loans.forEach(function(doc){
+    ended = doc.hasOwnProperty('endDate')
+    if(!ended){
+      const dueDate = doc.dueDate
+      const actualTime = new Date().getTime()
+      if(dueDate < actualTime){
+        updatedDocument = { ...doc, endDate: actualTime}
+        str = JSON.stringify(updatedDocument, null, 4); // (Optional) beautiful indented output.
+        console.log(str);
+        books_coll.updateOne({_id: updatedDocument.bookId},{$inc: {"countAvailable": 1}})
+        users_coll.updateOne({_id: updatedDocument.userId},{$inc: {"loanCount": -1}})
+        loans_coll.updateOne({_id: updatedDocument._id}, {$set: updatedDocument})
+      }
+    }
+  })
+  return 0
+}
